a2ade5dedfc1099c2621b3954c29182aae1f2de8
Andrew Mikaeel <mikandrew53@gmail.com>
2022-02-26 19:04:54 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Tic-Tac-Toe';
+export class AppComponent implements OnInit{
+  cells:Array<number> = []
+
+  ngOnInit(): void {
+      for(let i = 0; i < 9; i++)
+        this.cells.push(i);
+  }
 }
+
+
+
